47f51fe91dfe0183504b9b2fe2607b2b83d5d299
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-17 18:00:53 +1100

    Sticky header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import ThemeSwitch from "./ThemeSwitch";
 
 export default function Header() {
   return (
-    <header className="bg-white dark:bg-black shadow-sm dark:border-b dark:border-gray-800">
+    <header className="sticky top-0 z-50 bg-white dark:bg-black shadow-sm dark:border-b dark:border-gray-800">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center">
           <span className="text-xl dark:text-gray-100">Strata Committee Body Corporate</span>
