89717b1b379fac34f093b3a68f8ea5f3823df732
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-18 09:07:03 +1100

    New Webpage

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Link from 'next/link';
 
 const Contact = () => {
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold">Contact Page</h1>
         <p>This is the contact page linked from the homepage.</p>
+        <Link href="/">
+        <p className="text-blue-500 underline cursor-pointer mt-2">Back to Home</p>
+      </Link>
       </div>
     );
   };
