e0b7edcff21c62149374f3e55df9b33766e31099
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-17 18:05:04 +1100

    Sticky header

--- components/Features.tsx
+++ components/Features.tsx
@@ -41,7 +41,7 @@ const Features = () => {
   ];
 
   return (
-    <section className="container mx-auto px-4 py-12 bg-gray-50 dark:bg-gray-900 transition-colors duration-200 rounded-lg">
+    <section className="container mb-20 mx-auto px-4 py-12 bg-gray-50 dark:bg-gray-900 transition-colors duration-200 rounded-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">Mission</h2>
