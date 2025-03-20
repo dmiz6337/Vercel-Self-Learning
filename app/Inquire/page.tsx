79be064241128eae3c1eb987c90be232e26dd29a
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-20 13:39:24 +1100

    Inquiry Commit

--- app/Inquire/page.tsx
+++ app/Inquire/page.tsx
@@ -40,7 +40,7 @@ export default function InquiriesPage() {
     <div className="flex flex-col min-h-screen bg-white dark:bg-black">
         <Header />
         <main>
-            <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
+            <div className="max-w-md mx-auto mt-10 p-4 bg-black text-white dark:bg-white dark:text-black px-5 py-2 rounded-md text-base font-semibold hover:bg-gray-800 dark:hover:bg-gray-200 transition duration-300">
                 <h1 className="text-2xl font-bold mb-4">Post an Inquiry</h1>
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <textarea
@@ -62,6 +62,7 @@ export default function InquiriesPage() {
                     <p className="mt-4 text-center text-green-600">{responseMsg}</p>
                 )}
             </div>
+            <div className="h-20" ></div>
             <div className="flex justify-center space-x-6">
                 <Link
                 href="/"
