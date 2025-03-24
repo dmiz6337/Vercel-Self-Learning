2fe944adf8ef1d9308269c23686c9c2a8d217c80
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-24 17:46:40 +1100

    Info board Commit

---
+++ components/DownloadButton.tsx
@@ -0,0 +1,16 @@
+// components/DownloadButton.js
+import React from 'react';
+
+const DownloadButton = () => {
+  return (
+    <a
+      href="/example.pdf"  // Path to the document in the public folder
+      download="example.pdf"  // Optional: name the file when downloading
+      className="btn-download"
+    >
+      Download PDF
+    </a>
+  );
+};
+
+export default DownloadButton;
