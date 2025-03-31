9cb689ff9f1e855904a13785d2de230da6ee2fb2
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-03-31 23:14:26 +1100

    Payment page staging

--- app/api/pay/route.ts
+++ app/api/pay/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
     // Redirect the user to the payment success page
     const redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/payment-success`;
 
-    return NextResponse.redirect(redirectUrl);
+    return NextResponse.redirect(redirectUrl, 303);
 
   } catch (error: any) {
     console.error("Error during payment initiation:", error);
