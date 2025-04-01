806033cb99911cf390640f93df4d42c4f3c23ccf
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-04-01 12:13:14 +1100

    Deployment Test

--- app/api/daily-task/route.ts
+++ app/api/daily-task/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "nodejs"
-export const preferredRegion = ["syd1"]
 
 export async function GET(req: NextRequest) {
   console.log("Running daily task...");
