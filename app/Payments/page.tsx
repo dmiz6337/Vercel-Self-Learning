9c8ce81592a9313b0cfa6675f8984166e60e2a0c
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-04-05 08:41:07 +1100

    statics

--- app/Payments/page.tsx
+++ app/Payments/page.tsx
@@ -45,33 +45,31 @@ export default function PaymentPage() {
     >
         <Header />
         <main>
-            <div className="flex items-center justify-center min-h-screen dark:bg-black">
-                <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md text-center">
-                    <h1 className="text-2xl text-black font-bold mb-4">Payment Page</h1>
-                    <p className="text-gray-600 mb-6">Enter the amount you want to pay</p>
+              <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md text-center">
+                  <h1 className="text-2xl text-black font-bold mb-4">Payment Page</h1>
+                  <p className="text-gray-600 mb-6">Enter the amount you want to pay</p>
 
-                    <div className="mb-4">
-                        <input
-                            type="number"
-                            id="amount"
-                            value={amount}
-                            onChange={(e) => setAmount(Number(e.target.value))}
-                            placeholder="Enter amount"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                    </div>
+                  <div className="mb-4">
+                      <input
+                          type="number"
+                          id="amount"
+                          value={amount}
+                          onChange={(e) => setAmount(Number(e.target.value))}
+                          placeholder="Enter amount"
+                          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                      />
+                  </div>
 
-                    <button
-                    onClick={handlePayment}
-                    disabled={loading}
-                    className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition disabled:bg-gray-400"
-                    >
-                    {loading ? "Processing..." : "Pay Now"}
-                    </button>
+                  <button
+                  onClick={handlePayment}
+                  disabled={loading}
+                  className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition disabled:bg-gray-400"
+                  >
+                  {loading ? "Processing..." : "Pay Now"}
+                  </button>
 
-                    {error && <p className="text-red-500 mt-4">{error}</p>}
-                </div>
-            </div>
+                  {error && <p className="text-red-500 mt-4">{error}</p>}
+              </div>
         </main>
         <div className="h-20" ></div>
         <Footer />
