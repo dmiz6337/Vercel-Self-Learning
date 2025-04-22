cbcc5b6c8ccc84e34fec4bb902ecb209a3006a4d
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-04-22 11:47:23 +1000

    Test: Vercel PHP Strata Roll

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.unitOwner.createMany({
+  await prisma.UnitOwner.createMany({
     data: [
       {
         name: 'Alice Johnson',
