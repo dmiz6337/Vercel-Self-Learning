518159c732ab556f9fac0f2d8553c47463e50377
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-04-22 10:47:29 +1000

    Test: Vercel PHP Strata Roll

--- components/Header.tsx
+++ components/Header.tsx
@@ -56,6 +56,14 @@ export default function Header() {
             </li>
             <li>
               <Link
+                href="/api/strata-roll.php"
+                className="text-sm text-gray-800 dark:text-white px-4 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+              >
+                Strata Roll
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/Reviews"
                 className="text-sm text-gray-800 dark:text-white px-4 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
               >
