d626e54eda8df9c153ec348a24b5c8107e81e812
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-05-09 13:00:34 +1000

    PHP page test

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,6 @@ export default withAuth({
 export const config = {
     // Only allow homepage and NextAuth sign-in page to be accessible without authentication
     matcher: [
-        "/((?!$|api/auth/signin|_next/static|_next/image|statics|favicon.ico).*)"
-    ] // block everything except homepage and sign-in
+      "/((?!api/auth|api/auth/.*|_next/static|_next/image|statics|favicon.ico).*)"
+    ]
 };
