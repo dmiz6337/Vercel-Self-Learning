e5d1acc9a151c6ba7916a1955aafc16027ddb6c9
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-05-10 11:21:15 +1000

    Google Auth

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -8,7 +8,7 @@ export default function AuthButton() {
   if (session?.user) {
     return (
       <div>
-        <p>Signed in as {session.user.name}</p>
+        <p className="text-xs">Signed in as {session.user.name}</p>
         <button onClick={() => signOut()}>Sign out</button>
       </div>
     );
