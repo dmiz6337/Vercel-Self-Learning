a41474e7c246ad3786384691b5357f647ef8293d
daniel mizoguchi <dmiz6337@uni.sydney.edu.au>
2025-05-13 10:35:25 +1000

    Google Auth

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,25 +1,37 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function AuthButton() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   if (session?.user) {
     return (
       <div>
         <p className="text-xs">Signed in as {session.user.name}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <button
+          onClick={async () => {
+            await signOut({ redirect: false });
+            router.push("/"); // Redirect to home after sign out
+          }}
+        >
+          Sign out
+        </button>
       </div>
     );
   }
 
-  // If not signed in, show Google sign-in button
   return (
-    <button onClick={() => signIn('google', { callbackUrl: process.env.NEXT_PUBLIC_API_URL})}>
+    <button
+      onClick={() =>
+        signIn("google", {
+          callbackUrl: process.env.NEXT_PUBLIC_API_URL || "/",
+        })
+      }
+    >
       Sign in with Google
     </button>
   );
-
-  return <button onClick={() => signIn()}>Sign in</button>;
 }
